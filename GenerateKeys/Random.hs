a0994bc0b46f66683292e1ca3d86e022ad8f222b
Ishita Takeshi <ilovehumanandvocaloidno01@gmail.com>
2013-07-23 15:22:37 +0900

    Updated to use a hash function in Random.hs

--- GenerateKeys/Random.hs
+++ GenerateKeys/Random.hs
@@ -6,11 +6,12 @@ module Random
 import System.Entropy
 import System.Random
 import Data.Time.Clock.POSIX
+import Crypto.Hash.SHA512
 
 genSeed :: IO Integer
 genSeed = do
 	mbstr <- getEntropy 128
-	let str' = show mbstr	
+	let str' = show $ hash mbstr	
 	let str = map toEnum $ filter (\x -> 48<=x && x<58) $ map fromEnum $ str':: [Char]
 	let randomNum = read str :: Integer
 	return randomNum
